1fdcce98a39589449c7ca7a4e2c88ebf68280e09
JayfordMartinez-UCM <jayfordmartinez131@gmail.com>
2025-08-29 18:55:16 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+/**
+ * This is a documentation comment.
+ * It explains what the program does.
+ */
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is a single-line comment
+        System.out.println("Hello, this shows single-line comments.");
+        
+        /*
+         This is a multi-line comment
+         It can take up more than one line
+        */
+        System.out.println("This shows multi-line comments.");
+        
+        System.out.println("The text at the top is a documentation comment.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+System.out.println("Easy come, easy go, that's just how you live");
+System.out.println("Take, take, take it all, but you never give");
+System.out.println("Should've known you was trouble from the first kiss");
+System.out.println("Had your eyes wide open, why were they open?");
+}
+}
