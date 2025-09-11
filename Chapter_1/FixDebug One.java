f899a0266c3b90dfb5d8e69fd22438557d387966
JayfordMartinez-UCM <jayfordmartinez131@gmail.com>
2025-09-11 22:18:57 +0800

    Add files via upload

---
+++ Chapter_1/FixDebug One.java
@@ -0,0 +1,21 @@
+public class FixDebug One
+{
+   /* This program displays a greeting */
+   public static void Main(String args)   
+   {
+      Systm.out.println("Hello World!")         
+   }
+
+/*
+List down the spotted bugs below:
+
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+
+Bugs:
+1.Wrong class name syntax - "FixDebug One" has a space. Should be "FixDebugOne".
+2.Wrong method name - "Main" should be lowercase "main".
+3.Wrong parameter - "String args" should be "String[] args".
+4.Misspelled "System" - written as "Systm".
+5.Missing semicolon after println statement.
+*/
